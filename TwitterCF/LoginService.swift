5fdbee0d7127a8fabafee8c3da2bd10813a2dea6
Cisco <franciscoraglandjr@gmail.com>
2015-10-27 23:53:20 -0700

    first commit

---
+++ TwitterCF/LoginService.swift
@@ -0,0 +1,9 @@
+//
+//  LoginService.swift
+//  TwitterCF
+//
+//  Created by Francisco Ragland Jr on 10/27/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+import Foundation
---
+++ TwitterCF/TweetJSONParser.swift
@@ -0,0 +1,9 @@
+//
+//  TweetJSONParser.swift
+//  TwitterCF
+//
+//  Created by Francisco Ragland Jr on 10/26/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+import Foundation
